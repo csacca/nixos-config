f7d3033ae25b32dbd72c5e0f70248908740ac80b
Chris Sacca <csacca@csacca.net>
2024-03-05 21:33:23 -0500

    remove i915.fastboot

--- modules/core/common/system/hardware/cpu/intel/default.nix
+++ modules/core/common/system/hardware/cpu/intel/default.nix
@@ -12,7 +12,7 @@ in {
     hardware.cpu.intel.updateMicrocode = true;
     boot = {
       kernelModules = ["kvm-intel"];
-      kernelParams = ["i915.fastboot=1" "enable_gvt=1"];
+      kernelParams = [];
     };
 
     environment.systemPackages = with pkgs; [intel-gpu-tools];
