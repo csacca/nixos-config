0fff2c494589feac4970fd0c84cd7f6fa51a2f78
Chris Sacca <csacca@csacca.net>
2024-03-07 21:05:31 -0500

    enable themes

--- modules/core/common/system/os/boot/default.nix
+++ modules/core/common/system/os/boot/default.nix
@@ -3,6 +3,6 @@
     ./loaders # per-bootloader configurations
     # ./secure-boot.nix # secure boot module
     ./generic.nix # generic configuration, such as kernel and tmpfs setup
-    # ./plymouth.nix # plymouth boot splash
+    ./plymouth.nix # plymouth boot splash
   ];
 }
