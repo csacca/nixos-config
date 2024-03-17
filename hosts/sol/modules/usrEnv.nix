259786e16bdbe3788b178f8096d5495ac78dbe71
Chris Sacca <csacca@csacca.net>
2024-03-17 16:30:36 -0400

    enable launchers

--- hosts/sol/modules/usrEnv.nix
+++ hosts/sol/modules/usrEnv.nix
@@ -2,6 +2,10 @@
   config.modules.usrEnv = {
     desktop = "Hyprland";
     useHomeManager = true;
+    launchers = {
+      anyrun.enable = true;
+      tofi.enable = true;
+    };
 
     screenlock.hyprlock.enable = true;
   };
